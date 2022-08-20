d7c895f6c38c9e42ecdf8fd87c735bf2732363aa
prokash <pprokash102@gmail.com>
2022-08-20 20:37:11 +0600

    todo app create and setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import notes from "../../assets/images/notes.png";
+import plusImg from "../../assets/images/plus.png";
+import dabbleTik from "../../assets/images/double-tick.png";
+export default function Header() {
+  return (
+    <header>
+      <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+        <img src={notes} className="w-6 h-6" alt="Add todo" />
+        <input
+          type="text"
+          placeholder="Type your todo"
+          className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+        />
+        <button
+          style={{ backgroundImage: `url(${plusImg})` }}
+          type="submit"
+          className={`appearance-none w-8 h-8  bg-no-repeat bg-contain`}
+        ></button>
+      </form>
+
+      <ul className="flex justify-between my-4 text-xs text-gray-500">
+        <li className="flex space-x-1 cursor-pointer">
+          <img className="w-4 h-4" src={dabbleTik} alt="Complete" />
+          <span>Complete All Tasks</span>
+        </li>
+        <li className="cursor-pointer">Clear completed</li>
+      </ul>
+    </header>
+  );
+}
